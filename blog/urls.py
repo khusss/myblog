f6c8eb861e6ad9cc69f59dbe007bb6de3200bf34
khusss <khudd@naver.com>
2019-02-12 18:55:47 +0900

    template adjust

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path(r'', views.post_list, name='post_list'),
+    path(r'', views.index, name='index'),
+    # path(r'', views.post_list, name='post_list'),
     path(r'post/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
     # path('',views.post_detail,name='post_detail'),
     path(r'post/new/$', views.post_new, name='post_new'),
@@ -14,4 +15,5 @@ urlpatterns = [
     path(r'post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
     path(r'comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
     path(r'comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,19 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .forms import PostForm,CommentForm
 from django.contrib.auth.decorators import login_required
+from django.template import loader
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_question_list': "test",
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
